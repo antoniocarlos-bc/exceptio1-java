9290786c08838b44985a926465adc86a848fc9d1
antoniocarlos-bc <antoniocarlos_bc@outlook.com>
2020-11-18 12:01:48 -0300

    Bad solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -47,11 +47,24 @@ public class Reservation {
 		 return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	} 
  
-	public void updateDates(Date checkin, Date checkout)
+	public String updateDates(Date checkin, Date checkout)
 	{
+		 Date now = new Date();
+		if(checkin.before(now) || checkout.before(now))
+		   {
+			   return "Erro in reservation: Reservation dates for update must be future dates";
+			   
+		   }
+		if(!checkout.after(checkin))
+			{
+				return "Erro in reservation: check-out date must be agter check-in date";
+				
+			}
 		
 		this.checkin = checkin;
 		this.checkout = checkout;
+		
+		return null;
 		
 		
 	}
